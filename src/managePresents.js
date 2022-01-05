94ff0aea4148fd9593a0e425d996853f0b2acf45
Saloni <salonimehta2127@gmail.com>
2022-01-04 23:46:03 -0500

    Completed

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,15 @@
 export function managePresents(state, action) {
   // your code here
+  // set  an default value for the state argument
+  //- an object with a key
+  // number of presents assigned to 0 {numberOfPresents:0}
+  // action={
+  //   type:"presents/increase",
+  // }
+  if (action.type==="presents/increase"){
+    return {numberOfPresents:state.numberOfPresents+1}
+  }
+  else{
+    return state;
+  }
 }
